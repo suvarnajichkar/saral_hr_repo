8bd542cfab376d2d8d40defbdd1315204e383405
suvarnajichkar <suvarnajichkar204@gmail.com>
2026-02-09 07:27:58 +0000

    Remove month dropdown and duplicates

--- saral_hr/saral_hr/doctype/salary_component/salary_component.js
+++ saral_hr/saral_hr/doctype/salary_component/salary_component.js
@@ -1,9 +1,15 @@
 frappe.ui.form.on('Salary Component', {
-	is_special_component: function(frm) {
+	refresh(frm) {
+		if (frm.doc.is_special_component) {
+			lock_month_table(frm);
+		}
+	},
+
+	is_special_component(frm) {
 		if (frm.doc.is_special_component) {
 			populate_all_months(frm);
+			lock_month_table(frm);
 		} else {
-			// Clear the table if unchecked
 			frm.clear_table('enter_amount_according_to_months');
 			frm.refresh_field('enter_amount_according_to_months');
 		}
@@ -15,19 +21,29 @@ function populate_all_months(frm) {
 		"January", "February", "March", "April", "May", "June",
 		"July", "August", "September", "October", "November", "December"
 	];
-	
-	// Get existing months
-	let existing_months = frm.doc.enter_amount_according_to_months.map(row => row.month);
-	
-	// Add missing months
+
+	let existing_months = (frm.doc.enter_amount_according_to_months || [])
+		.map(r => r.month);
+
 	months.forEach(month => {
 		if (!existing_months.includes(month)) {
 			let row = frm.add_child('enter_amount_according_to_months');
 			row.month = month;
-			row.amount = 0.0;
+			row.amount = 0;
 		}
 	});
-	
-	// Refresh the field to show the table
+
 	frm.refresh_field('enter_amount_according_to_months');
+}
+
+function lock_month_table(frm) {
+	const grid = frm.get_field('enter_amount_according_to_months').grid;
+
+	// 🔒 stop add row
+	grid.cannot_add_rows = true;
+
+	// 🔒 hide UI buttons
+	grid.wrapper.find('.grid-add-row').hide();
+	grid.wrapper.find('.grid-remove-rows').hide();
+	grid.wrapper.find('.grid-row-check').hide();
 }
